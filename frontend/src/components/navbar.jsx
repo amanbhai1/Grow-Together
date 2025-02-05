b1d51db47ab9f80f2523c64a796429a8ee6a490b
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2025-02-05 22:40:15 +0530

    commit

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../redux/actions.js";
 import { FiUser } from "react-icons/fi";
 import { PiStudent } from "react-icons/pi";
@@ -9,11 +9,15 @@ import { IoIosSearch } from "react-icons/io";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { MdOutlineLogout, MdMenu, MdClose } from "react-icons/md";
 import GrowTogetherLogo from '../assets/growTogetherLogo.png';
+import { use } from "react";
 
 const Navbar = (item) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const user = useSelector(state => state.user);
+  console.log(user);
 
   const activeNavItem = item.page;
 
@@ -41,60 +45,99 @@ const Navbar = (item) => {
 
         {/* Desktop Navigation Links */}
         <div className="hidden md:flex items-center space-x-8">
-          <NavItem
-            active={activeNavItem === "tutor"}
-            onClick={() => handleClick("/tutor")}
-            tooltip="Teach"
-            Icon={LiaChalkboardTeacherSolid}
-          />
-          <NavItem
-            active={activeNavItem === "learner"}
-            onClick={() => handleClick("/learner")}
-            tooltip="Learn"
-            Icon={PiStudent}
-          />
-          <NavItem
+          {
+            (user.isLoggedIn && user.user.isAdmin) ? (<>
+              <NavItem
+                active={activeNavItem === "tutor"}
+                onClick={() => handleClick("/tutor")}
+                tooltip="Teach"
+                Icon={LiaChalkboardTeacherSolid}
+              /></>) : <></>
+          }
+          {
+            (user.isLoggedIn && user.user.isAdmin) ? (<>
+            </>) : <>
+              <NavItem
+                active={activeNavItem === "learner"}
+                onClick={() => handleClick("/learner")}
+                tooltip="Learn"
+                Icon={PiStudent}
+              />
+            </>
+          }
+          {
+          (user.isLoggenIn && user.user.isAdmin) ? (<></>)
+          : (
+            <NavItem
             active={activeNavItem === "search"}
             onClick={() => handleClick("/search")}
             tooltip="Search"
             Icon={IoIosSearch}
           />
+          )
+          }
+         
           <NavItem
             active={activeNavItem === "notification"}
             onClick={() => handleClick("/notification")}
             tooltip="Notification"
             Icon={IoMdNotificationsOutline}
           />
-          <a
-            href="#"
+          {
+            (user.isLoggedIn && user.user.isAdmin) ? (<></>)
+            : (
+              <a
+            href="/signup"
             className="text-gray-700 hover:text-teal-600 transition-all font-medium"
           >
             Become a Mentor
           </a>
+            )
+          }
+      
           <a
-            href="#"
+            href="/"
             className="text-gray-700 hover:text-teal-600 transition-all font-medium"
           >
             Find a Mentor
           </a>
-          <button
-            className="px-4 py-2 text-white bg-teal-600 rounded-lg hover:bg-teal-700 transition-all shadow-md hover:shadow-lg"
-            onClick={() => handleClick("/signup")}
-          >
-            Signup
-          </button>
-          <button
-            className="px-4 py-2 text-teal-600 border border-teal-600 rounded-lg hover:bg-teal-600 hover:text-white transition-all shadow-md hover:shadow-lg"
-            onClick={() => handleClick("/login")}
-          >
-            Login
-          </button>
-          <NavItem
-            active={activeNavItem === "profile"}
-            onClick={() => handleClick("/profile")}
-            tooltip="Profile"
-            Icon={FiUser}
-          />
+          <div className="text-teal-500 font-semibold">
+            {(user.isLoggedIn) ? user.user.name : ''}
+          </div>
+          {(user.isLoggedIn) ? (
+            <button
+              className="px-4 py-2 bg-teal-600 hover:bg-teal-800 text-white font-bold rounded"
+              onClick={() => handleLogout()}>Logout</button>
+          ) : (
+            <>
+              <button
+                className="px-4 py-2 text-white bg-teal-600 rounded-lg hover:bg-teal-700 transition-all shadow-md hover:shadow-lg"
+                onClick={() => handleClick("/signup")}
+              >
+                Signup
+              </button>
+              <button
+                className="px-4 py-2 text-teal-600 border border-teal-600 rounded-lg hover:bg-teal-600 hover:text-white transition-all shadow-md hover:shadow-lg"
+                onClick={() => handleClick("/login")}
+              >
+                Login
+              </button>
+            </>
+          )
+          }
+          {
+            (user.isLoggedIn) ? (
+              <NavItem
+                active={activeNavItem === "profile"}
+                onClick={() => handleClick("/profile")}
+                tooltip="Profile"
+                Icon={FiUser}
+              />)
+              : <>
+              </>
+
+          }
+
         </div>
 
         {/* Mobile Menu Toggle Button */}
@@ -188,11 +231,10 @@ const NavItem = ({ active, onClick, tooltip, Icon }) => {
   return (
     <div className="relative group flex flex-col items-center">
       <button
-        className={`p-2 rounded-full transition-all ${
-          active
-            ? "text-teal-600 scale-95 shadow-[0_0_7px_rgb(0,128,128)]"
-            : "text-gray-700 hover:text-teal-600 hover:scale-95 hover:shadow-[0_0_3px_rgb(173,216,230)]"
-        }`}
+        className={`p-2 rounded-full transition-all ${active
+          ? "text-teal-600 scale-95 shadow-[0_0_7px_rgb(0,128,128)]"
+          : "text-gray-700 hover:text-teal-600 hover:scale-95 hover:shadow-[0_0_3px_rgb(173,216,230)]"
+          }`}
         onClick={onClick}
       >
         <Icon className="text-2xl" />
@@ -201,9 +243,8 @@ const NavItem = ({ active, onClick, tooltip, Icon }) => {
         {tooltip}
       </span>
       <div
-        className={`w-1 h-[70%] ${
-          active ? "bg-teal-600 shadow-[0_0_15px_teal]" : "bg-transparent"
-        } rounded-full transition-all`}
+        className={`w-1 h-[70%] ${active ? "bg-teal-600 shadow-[0_0_15px_teal]" : "bg-transparent"
+          } rounded-full transition-all`}
       ></div>
     </div>
   );
@@ -213,11 +254,10 @@ const NavItem = ({ active, onClick, tooltip, Icon }) => {
 const MobileNavItem = ({ active, onClick, text, Icon }) => {
   return (
     <button
-      className={`flex items-center space-x-2 p-2 w-full text-left rounded-lg transition-all ${
-        active
-          ? "text-teal-600 bg-teal-50 shadow-md"
-          : "text-gray-700 hover:text-teal-600 hover:bg-gray-100"
-      }`}
+      className={`flex items-center space-x-2 p-2 w-full text-left rounded-lg transition-all ${active
+        ? "text-teal-600 bg-teal-50 shadow-md"
+        : "text-gray-700 hover:text-teal-600 hover:bg-gray-100"
+        }`}
       onClick={onClick}
     >
       <Icon className="text-xl" />
--- frontend/src/pages/search.jsx
+++ frontend/src/pages/search.jsx
@@ -1,35 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { MdOutlineStar } from "react-icons/md";
-import { CiSearch } from "react-icons/ci";
+import { MdOutlineStar } from 'react-icons/md';
+import { CiSearch } from 'react-icons/ci';
 import { useSelector } from 'react-redux';
 import toast, { Toaster } from 'react-hot-toast';
-// Dropdown banaya tha library se ui theek nhi hai
-// import { Listbox } from "@headlessui/react";
-// import { ChevronDown } from "lucide-react";
-
-// const options = [
-// "All",
-// "Engineering",
-// "Programming",
-// "Maths",
-// "Science",
-// "Sports",
-// "Music",
-// "Art",
-// "Busisness",
-// "Cooking",
-// "Crafting",
-// "Fashion",
-// "Fitness",
-// "Gaming",
-// "Language",
-// "Literature",
-// "Technology",
-// "Soft Skills",
-// "Photograpy",
-// "Hobbies"];
 
 const Search = () => {
   const navigate = useNavigate();
@@ -43,19 +18,24 @@ const Search = () => {
   }
 
   const getSearchResult = async (filter) => {
-    axios.post('http://localhost:5000/api/getSearchResult', { filter: filter, category: cat }, {
-      headers: {
-        Authorization: `Bearer ${user.token}`
-      }
-    })
-      .then(res => {
+    axios
+      .post(
+        'http://localhost:5000/api/getSearchResult',
+        { filter: filter, category: cat },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      )
+      .then((res) => {
         if (res.status === 200) {
           setResults(res.data.result);
         } else {
           toast.error('Server error');
         }
       })
-      .catch(err => {
+      .catch((err) => {
         toast.error('Something went wrong!');
       });
   };
@@ -66,11 +46,18 @@ const Search = () => {
 
   return (
     <div className="flex justify-start items-center mt-8 w-full h-full">
-      <div><Toaster /></div>
+      <div>
+        <Toaster />
+      </div>
       <div className="flex flex-col justify-start items-center w-full h-full">
+        {/* Search Bar */}
         <div className="flex justify-center items-center w-full h-20">
-          <div className="flex bg-white shadow-md pr-4 rounded-l-3xl rounded-r-3xl w-1/2">
-            <select className="bg-teal-100 px-8 py-3 border-none rounded-l-3xl appearance-none-none" value={cat} onChange={(e) => setCat(e.target.value)}>
+          <div className="flex bg-white shadow-md pr-4 rounded-3xl w-full md:w-1/2">
+            <select
+              className="bg-teal-100 px-4 md:px-8 py-3 border-none rounded-l-3xl appearance-none"
+              value={cat}
+              onChange={(e) => setCat(e.target.value)}
+            >
               <option value="All">All Category</option>
               <option value="Engineering">Engineering</option>
               <option value="Programming">Programming</option>
@@ -92,65 +79,65 @@ const Search = () => {
               <option value="Photography">Photography</option>
               <option value="Hobbies">Hobbies</option>
             </select>
-            {/* <Listbox value={cat} onChange={(e) => setCat(e.target.value)}>
-              <div className="relative w-60">
-                <Listbox.Button className="flex justify-between items-center bg-[#40ccc0] px-5 py-3 border-none rounded-l-3xl w-full">
-                  {cat}
-                  <ChevronDown className="w-5 h-5 text-gray-500" />
-                </Listbox.Button>
-
-                <Listbox.Options className="absolute border-gray-300 bg-[#40ccc0] shadow-lg backdrop-blur-sm mt-1 border rounded-lg w-full overflow-hidden">
-                  {options.map((option, index) => (
-                    <Listbox.Option key={index} value={option} className="hover:bg-gray-100 p-3 cursor-pointer">
-                      {option}
-                    </Listbox.Option>
-                  ))}
-                </Listbox.Options>
-              </div>
-            </Listbox> */}
             <input
               type="text"
               placeholder="Search for a course"
               value={filter}
-              onChange={(e) => setFilter(e.target.value)}
+              onChange={(e) => {
+                setFilter(e.target.value);
+                getSearchResult(filter);
+              }}
               className="bg-transparent pl-5 border-none w-full h-10 text-black outline-none"
             />
             <button
-              onClick={(e) => { e.preventDefault(); getSearchResult(filter); }}
-              className="border-0 bg-transparent rounded-full w-10 h-10 text-black">
+              onClick={(e) => {
+                e.preventDefault();
+                getSearchResult(filter);
+              }}
+              className="border-0 bg-transparent rounded-full w-10 h-10 text-black"
+            >
               <CiSearch className="mr-2 text-2xl" />
             </button>
           </div>
         </div>
-        <ul className="gap-1 grid grid-cols-3 mt-4 p-0 w-5/6 overflow-y-auto list-none">
+
+        {/* Results Grid */}
+        <ul className="gap-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-4 p-0 w-5/6 overflow-y-auto list-none">
           {results.map((item, index) => (
             <div
-              className="relative hover:brightness-95 flex flex-row items-start gap-4 shadow-md hover:shadow-xl hover:shadow-teal-100/50 m-2 mb-2 p-2 border rounded-lg hover:scale-95 transition-transform duration-300"
+              className="relative hover:brightness-95 flex flex-col sm:flex-row items-start gap-4 shadow-md hover:shadow-xl hover:shadow-teal-100/50 m-2 mb-2 p-2 border rounded-lg hover:scale-95 transition-transform duration-300"
               key={index}
               onClick={() => handleTile(item.courseId)}
             >
               <img
                 src={`/images/${item.category}.jpg`}
-                className="rounded-md w-1/2 h-full object-cover"
+                className="rounded-md w-full sm:w-1/2 h-48 sm:h-full object-cover"
                 loading="lazy"
+                alt={item.courseName}
               />
-              <p className="bottom-2 left-2 absolute flex justify-center items-center border-[rgba(200,109,223,0.633)] bg-red-200 px-4 py-2 border rounded-full text-lg">
-                {item.rating}<MdOutlineStar className="text-xs" />
+              <p className="absolute top-2 right-2 flex justify-center items-center border-[rgba(200,109,223,0.633)] bg-red-200 px-4 py-2 border rounded-full text-lg">
+                {item.rating}
+                <MdOutlineStar className="text-xs" />
               </p>
               <div className="flex items-end p-2 w-full">
                 <div className="flex flex-col gap-2">
-                  <p className="font-sans font-semibold text-2xl text-teal-700">{item.courseName}</p>
-                  <p className="text-red-300 text-xl">By {item.courseTutor}</p>
-                  <p className="line-clamp-5 text-lg">{item.courseDesc}</p>
+                  <p className="font-sans font-semibold text-xl sm:text-2xl text-teal-700">
+                    {item.courseName}
+                  </p>
+                  <p className="text-red-300 text-lg sm:text-xl">
+                    By {item.courseTutor}
+                  </p>
+                  <p className="line-clamp-3 text-base sm:text-lg">
+                    {item.courseDesc}
+                  </p>
                 </div>
               </div>
             </div>
           ))}
-
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default Search;
