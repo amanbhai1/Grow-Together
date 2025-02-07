66e321a732f2587a4e49cdb208a6ec0599798ba6
Kushal <kushalsharmahacker678@gmail.com>
2025-02-07 11:44:14 +0530

    LinkAiMentor

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -37,15 +37,15 @@ const Navbar = (item) => {
   };
 
   return (
-    <nav className="mt-18 top-0 left-0 w-full bg-white shadow-lg z-50">
-      <div className="container mx-auto px-4 py-3 flex items-center justify-between">
+    <nav className="top-0 left-0 z-50 bg-white shadow-lg mt-18 w-full">
+      <div className="flex justify-between items-center mx-auto px-4 py-3 container">
         {/* Logo Section */}
         <div className="flex items-center space-x-3">
           <img className="w-48" src={GrowTogetherLogo} alt="Grow Together Logo" />
         </div>
 
         {/* Desktop Navigation Links */}
-        <div className="hidden md:flex items-center space-x-8">
+        <div className="md:flex items-center space-x-8 hidden">
           {
             (user.isLoggedIn && user.user.isAdmin) ? (<>
               <NavItem
@@ -89,7 +89,7 @@ const Navbar = (item) => {
               : (
                 <a
                   href="/signup"
-                  className="text-gray-700 hover:text-teal-600 transition-all font-medium"
+                  className="font-medium text-gray-700 hover:text-teal-600 transition-all"
                 >
                   Become a Mentor
                 </a>
@@ -98,27 +98,33 @@ const Navbar = (item) => {
 
           <a
             href="/"
-            className="text-gray-700 hover:text-teal-600 transition-all font-medium"
+            className="font-medium text-gray-700 hover:text-teal-600 transition-all"
           >
             Find a Mentor
           </a>
-          <div className="text-teal-500 font-semibold">
+          <a
+            href="/ai-feedback"
+            className="bg-gradient-to-r from-blue-600 hover:from-blue-700 to-purple-600 hover:to-purple-700 p-2 rounded-lg font-medium text-white transition-all"
+          >
+            AI Mentor 
+          </a>
+          <div className="font-semibold text-teal-500">
             {(user.isLoggedIn) ? user.user.name : ''}
           </div>
           {(user.isLoggedIn) ? (
             <button
-              className="px-4 py-2 bg-teal-600 hover:bg-teal-800 text-white font-bold rounded"
+              className="bg-teal-600 hover:bg-teal-800 px-4 py-2 rounded font-bold text-white"
               onClick={() => handleLogout()}>Logout</button>
           ) : (
             <>
               <button
-                className="px-4 py-2 text-white bg-teal-600 rounded-lg hover:bg-teal-700 transition-all shadow-md hover:shadow-lg"
+                className="bg-teal-600 hover:bg-teal-700 shadow-md hover:shadow-lg px-4 py-2 rounded-lg text-white transition-all"
                 onClick={() => handleClick("/signup")}
               >
                 Signup
               </button>
               <button
-                className="px-4 py-2 text-teal-600 border border-teal-600 rounded-lg hover:bg-teal-600 hover:text-white transition-all shadow-md hover:shadow-lg"
+                className="hover:bg-teal-600 shadow-md hover:shadow-lg px-4 py-2 border border-teal-600 rounded-lg text-teal-600 hover:text-white transition-all"
                 onClick={() => handleClick("/login")}
               >
                 Login
@@ -157,8 +163,8 @@ const Navbar = (item) => {
 
         {/* Mobile Menu */}
         {isMobileMenuOpen && (
-          <div className="md:hidden absolute h-[60vh] sm:w-1/2 w-4/5 top-24 z-10 left-0 bg-white shadow-lg">
-            <div className="flex flex-col gap-6 px-4 items-center">
+          <div className="top-24 left-0 z-10 absolute md:hidden bg-white shadow-lg w-4/5 sm:w-1/2 h-[60vh]">
+            <div className="flex flex-col items-center gap-6 px-4">
               {
                 (user.isLoggedIn && user.user.isAdmin) ? (<>
                   <NavItem
@@ -202,7 +208,7 @@ const Navbar = (item) => {
                   : (
                     <a
                       href="/signup"
-                      className="text-gray-700 hover:text-teal-600 transition-all font-medium"
+                      className="font-medium text-gray-700 hover:text-teal-600 transition-all"
                     >
                       Become a Mentor
                     </a>
@@ -211,27 +217,27 @@ const Navbar = (item) => {
 
               <a
                 href="/"
-                className="text-gray-700 hover:text-teal-600 transition-all font-medium"
+                className="font-medium text-gray-700 hover:text-teal-600 transition-all"
               >
                 Find a Mentor
               </a>
-              <div className="text-teal-500 font-semibold">
+              <div className="font-semibold text-teal-500">
                 {(user.isLoggedIn) ? user.user.name : ''}
               </div>
               {(user.isLoggedIn) ? (
                 <button
-                  className="px-4 py-2 bg-teal-600 hover:bg-teal-800 text-white font-bold rounded"
+                  className="bg-teal-600 hover:bg-teal-800 px-4 py-2 rounded font-bold text-white"
                   onClick={() => handleLogout()}>Logout</button>
               ) : (
                 <>
                   <button
-                    className="px-4 py-2 text-white bg-teal-600 rounded-lg hover:bg-teal-700 transition-all shadow-md hover:shadow-lg"
+                    className="bg-teal-600 hover:bg-teal-700 shadow-md hover:shadow-lg px-4 py-2 rounded-lg text-white transition-all"
                     onClick={() => handleClick("/signup")}
                   >
                     Signup
                   </button>
                   <button
-                    className="px-4 py-2 text-teal-600 border border-teal-600 rounded-lg hover:bg-teal-600 hover:text-white transition-all shadow-md hover:shadow-lg"
+                    className="hover:bg-teal-600 shadow-md hover:shadow-lg px-4 py-2 border border-teal-600 rounded-lg text-teal-600 hover:text-white transition-all"
                     onClick={() => handleClick("/login")}
                   >
                     Login
@@ -262,7 +268,7 @@ const Navbar = (item) => {
 // Desktop NavItem Component
 const NavItem = ({ active, onClick, tooltip, Icon }) => {
   return (
-    <div className="relative group flex flex-col items-center">
+    <div className="group relative flex flex-col items-center">
       <button
         className={`p-2 rounded-full transition-all ${active
           ? "text-teal-600 scale-95 shadow-[0_0_7px_rgb(0,128,128)]"
@@ -272,7 +278,7 @@ const NavItem = ({ active, onClick, tooltip, Icon }) => {
       >
         <Icon className="text-2xl" />
       </button>
-      <span className="absolute bottom-[110%] left-1/2 -translate-x-1/2 px-2 py-1 text-xs bg-gray-700 text-white rounded opacity-0 transition-all group-hover:opacity-100">
+      <span className="bottom-[110%] left-1/2 absolute bg-gray-700 opacity-0 group-hover:opacity-100 px-2 py-1 rounded text-white text-xs transition-all -translate-x-1/2">
         {tooltip}
       </span>
       <div
